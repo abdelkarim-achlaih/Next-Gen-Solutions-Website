0a1a2bcd91fcdd4732daae31060bb9cc13d2612e
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-01 09:18:48 +0100

    Add scroll smoother for anchor links

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,7 +1,24 @@
 const lenis = new Lenis();
+
 function raf(time) {
 	lenis.raf(time);
 	requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
+
+const sectionNavs = document.querySelectorAll(".sections-nav li a");
+const stepsNavs = document.querySelectorAll(".steps-nav li a");
+
+scrollNavs(sectionNavs);
+scrollNavs(stepsNavs);
+
+function scrollNavs(navs) {
+	navs.forEach((nav) => {
+		nav.addEventListener("click", (e) => {
+			options = {
+				duration: 5,
+			};
+			lenis.scrollTo(nav.getAttribute("href"), options);
+		});
+	});
+}
