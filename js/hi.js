249fb0c7287b12224cac94312414af24b1de8a00
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-04 17:59:41 +0100

    Toggle icon: plus & minus

--- js/hi.js
+++ js/hi.js
@@ -5,5 +5,10 @@ mobileStepsTogglers.forEach((toogler) => {
 	toogler.onclick = (_) => {
 		toogler.classList.toggle("active");
 		toogler.nextElementSibling.classList.toggle("show");
+		if (toogler.classList.contains("active")) {
+			toogler.children[1].innerHTML = `<i class="fa-solid fa-minus display-5"></i>`;
+		} else {
+			toogler.children[1].innerHTML = `<i class="fa-solid fa-plus display-5"></i>`;
+		}
 	};
 });
