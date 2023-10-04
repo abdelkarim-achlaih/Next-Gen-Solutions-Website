4c430a675fc3641e7c85920438d0d185a3f3ffa7
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-04 18:40:44 +0100

    Steps mobile logic on different media sizes

--- js/main.js
+++ js/main.js
@@ -287,6 +287,21 @@ function navsStepsPinAnimation(step, pinType = "fixed") {
 
 // ------------------------------------------- Steps Container Mobile Logic
 
+let mobileStepsTogglers = document.querySelectorAll(
+	".steps-container-mobile .step-mobile-toogler"
+);
+mobileStepsTogglers.forEach((toogler) => {
+	toogler.onclick = (_) => {
+		toogler.classList.toggle("active");
+		toogler.nextElementSibling.classList.toggle("show");
+		if (toogler.classList.contains("active")) {
+			toogler.children[1].innerHTML = `<i class="fa-solid fa-minus display-5"></i>`;
+		} else {
+			toogler.children[1].innerHTML = `<i class="fa-solid fa-plus display-5"></i>`;
+		}
+	};
+});
+
 // ------------------------------------------- Studies + Contact Pin Animations
 let allowedWidth = 992;
 window.onload = (_) => {
