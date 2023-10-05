796600b46904040924a49fc79433eb1c7f26cd81
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-05 07:59:20 +0100

    Update hero sectio design

--- js/main.js
+++ js/main.js
@@ -2,61 +2,61 @@ gsap.registerPlugin(ScrollTrigger);
 
 // ------------------------------------------- Hero Animations
 
-let load = gsap.timeline({
-	defaults: {
-		duration: 0.3,
-	},
-});
-load
-	.from(".hero .navbar-brand", {
-		x: -2000,
-	})
-	.from(".hero .nav-link", {
-		y: -200,
-		stagger: 0.5,
-	})
-	.from(".hero .content h1", {
-		y: 250,
-		height: 0,
-		skewY: 20,
-		ease: "power2.out",
-	})
-	.from(".hero .content p", {
-		opacity: 0,
-		y: -50,
-	})
-	.to(".hero .content a", {
-		x: 0,
-	})
-	.from(".hero .gallery .box-1", {
-		x: 1000,
-		duration: 0.5,
-	})
-	.from(".hero .gallery .box-2", {
-		y: -1000,
-		duration: 0.5,
-	})
-	.from(".hero .gallery .box-3", {
-		y: 1000,
-		duration: 0.5,
-	})
-	.from(".hero .gallery .bg", {
-		height: "0%",
-	})
-	.from(
-		".hero .gallery div img",
-		{
-			opacity: 0,
-		},
-		"<"
-	)
-	.from(".hero .gallery div img", {
-		y: 500,
-	})
-	.to(".hero .gallery div img", {
-		scale: 1,
-		margin: 0,
-	});
+// let load = gsap.timeline({
+// 	defaults: {
+// 		duration: 0.3,
+// 	},
+// });
+// load
+// 	.from(".hero .navbar-brand", {
+// 		x: -2000,
+// 	})
+// 	.from(".hero .nav-link", {
+// 		y: -200,
+// 		stagger: 0.5,
+// 	})
+// 	.from(".hero .content h1", {
+// 		y: 250,
+// 		height: 0,
+// 		skewY: 20,
+// 		ease: "power2.out",
+// 	})
+// 	.from(".hero .content p", {
+// 		opacity: 0,
+// 		y: -50,
+// 	})
+// 	.to(".hero .content a", {
+// 		x: 0,
+// 	})
+// 	.from(".hero .gallery .box-1", {
+// 		x: 1000,
+// 		duration: 0.5,
+// 	})
+// 	.from(".hero .gallery .box-2", {
+// 		y: -1000,
+// 		duration: 0.5,
+// 	})
+// 	.from(".hero .gallery .box-3", {
+// 		y: 1000,
+// 		duration: 0.5,
+// 	})
+// 	.from(".hero .gallery .bg", {
+// 		height: "0%",
+// 	})
+// 	.from(
+// 		".hero .gallery div img",
+// 		{
+// 			opacity: 0,
+// 		},
+// 		"<"
+// 	)
+// 	.from(".hero .gallery div img", {
+// 		y: 500,
+// 	})
+// 	.to(".hero .gallery div img", {
+// 		scale: 1,
+// 		margin: 0,
+// 	});
 
 // ------------------------------------------- Navs Logic
 
