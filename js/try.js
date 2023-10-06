bad32ef581ea1e3263b5a3b64a70a60981a85834
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-06 17:00:38 +0100

    Adding header navs animation design in mobile

--- js/try.js
+++ js/try.js
@@ -3,5 +3,18 @@ let headerLinks = document.querySelectorAll("header .nav-item");
 let headerMobile = document.querySelector(".header-mobile");
 let links = "";
 headerLinks.forEach((link) => {
-	headerMobile.append(link.firstElementChild.cloneNode(true));
+	let div = document.createElement("div");
+	div.append(link.firstElementChild.cloneNode(true));
+	headerMobile.append(div);
 });
+let anim = gsap.from(".header-mobile div", {
+	duration: 1,
+	ease: Expo.easeInOut,
+	x: -500,
+	stagger: 0.5,
+});
+anim.pause();
+headerBtn.onclick = (_) => {
+	headerMobile.classList.toggle("show");
+	anim.restart();
+};
