259cdca8ced5295bb40f17d9a911067a80dd3bca
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-06 16:20:32 +0100

    Addin header navs design in mobile

--- js/try.js
+++ js/try.js
@@ -1,3 +1,7 @@
-window.onload = function () {
-	
-};
+let headerBtn = document.querySelector("header .btn-header");
+let headerLinks = document.querySelectorAll("header .nav-item");
+let headerMobile = document.querySelector(".header-mobile");
+let links = "";
+headerLinks.forEach((link) => {
+	headerMobile.append(link.firstElementChild.cloneNode(true));
+});
