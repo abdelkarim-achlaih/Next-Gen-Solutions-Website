03c380a00e8c1b0853cbb434986f6300e729e6cc
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-10-06 17:56:20 +0100

    Integrate the header in the main file and make its logic responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// ------------------------------------------- Header Button Logic
+
+let headerBtn = document.querySelector("header .btn-header");
+let headerLinks = document.querySelectorAll("header .nav-item");
+let headerMobile = document.querySelector(".header-mobile");
+headerLinks.forEach((link) => {
+	let div = document.createElement("div");
+	div.append(link.firstElementChild.cloneNode(true));
+	headerMobile.append(div);
+});
+let anim = gsap.from(".header-mobile div", {
+	duration: 1,
+	ease: Expo.easeInOut,
+	x: -500,
+	stagger: 0.5,
+});
+anim.pause();
+headerBtn.onclick = (_) => {
+	headerMobile.classList.toggle("show");
+	anim.restart();
+};
+
 // ------------------------------------------- Hero Animations
 
 function heroAnimationWide() {
